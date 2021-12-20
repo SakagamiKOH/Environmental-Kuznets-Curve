54f9c01dd9a3a08de62b8901154cb7bb3153385c
SakagamiKOH <sakagamikoh@gmail.com>
2021-12-20 11:23:25 +0900

    結合を以下のステップで実行 ①プライマリーキーを整理 ②プライマリーキーを基に結合

---
+++ 03_build/master/code/build.R
@@ -0,0 +1,44 @@
+main <- function(){
+  gdp_data <- read_interim("gdp", extension = "ready")
+  pollution_data <- read_interim("pollution", extension = "ready")
+
+  ##マージするためにプライマリーキーをそろえる(Type, 表記法など)
+  gdp_data_for_merge <- prep_gdp(gdp_data, interval = 5)
+
+  ##マージする
+  master_data <- prep_merge(
+    gdp_data_for_merge,
+    pollution_data)
+  
+  save_interim(master_data, "master")
+}
+
+
+##結合のためにプライマリーキーを整えたgdpデータを作成
+prep_gdp <- function(data_input, interval){
+  data_output <- data_input %>% 
+    dplyr::mutate(country_id = dplyr::if_else(country == "Japan", "JPN", "no code"),
+                  country_id = dplyr::if_else(student == "United States", "USA", country),
+                  ) %>% 
+    ##無駄な列を削除
+    dplyr::select( - country)
+  
+  return(data_output)
+}
+
+
+prep_merge <- function(gdp_data, pollution_data){
+  data_output <- gdp_data %>% 
+    ##by引数でプライマリーキーを対応付ける
+    dplyr::left_join(pollution_data, by = c("country_code" = "country",
+                                            "group_5years" = "year"))
+  
+  return(data_output)
+}
+
+
+
+
+source("01_admin/functions/basics.R")
+
+main()
