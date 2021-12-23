945d1fffc52940b5b5c30787ea194d03603f703b
SakagamiKOH <sakagamikoh@gmail.com>
2021-12-23 18:18:23 +0900

    summaarize_data():記述統計量の表を書く関数が解らん

--- 04_analyze/initial/code/analyze.R
+++ 04_analyze/initial/code/analyze.R
@@ -1,7 +1,137 @@
 main <- function(){
-  box::use(`functions`/basics)
-
+  my_folder <- "master"
+  ##データの読み込み
+  my_data <- read_interim(my_folder)
+  output_folder <- "initial"
+  
+  my_data %>% 
+    summarize_data()
+  
+  my_data %>% 
+    run_regression() %>% 
+    format_and_save_table(
+      my_file = "initial_reg",
+      my_title = "Initial regression",
+      my_varnames = main_varnames,
+      my_folder = my_folder
+    )
+  
+  my_data %>% 
+    ##make scatter polt grouped by group_var
+    run_scatter(
+      x_var = gdp_per_cap,
+      y_var = pollution,
+      group_var = country
+    ) %>% 
+    save_my_plot(var_name = "Environmenta Kuznets Curve",
+                 folder_name = my_folder)
 }
 
+
+run_regression <- function(data_input){
+  ##regressionの詳細を先に詰める
+  estimate_list <- list(
+    "OLS" = estimatr::lm_robust(
+      pollution ~ gdp_per_capita + I(gdp_per_capita^2),
+      clusters = country, se_type = "stata",
+      data = data_input
+    ),
+    
+    "Fixed Effect" = estimatr::lm_robust(
+      pollution ~ gdp_per_capita + I(gdp_per_capita^2),
+      fixed_effects = ~ country,
+      clusters = country, se_type = "stata",
+      data = data_input
+    )
+  )
+  
+  return(estimate_list)
+}
+
+
+format_and_save_table <- function(estimates_lists, my_file_name,
+                                  my_title, my_varnames, my_folder){
+  
+  ##formatとパスを指定
+  my_file_tex0 <- paste0(my_file_name, ".text")
+  my_file_html0 <- paste0(my_file_name, ".html")
+  my_file_tex <- here::here("04_analyze", my_folder,"table", my_file_tex0)
+  my_file_html <- here::here("04_analyze",my_folder,"table",my_file_html0)
+  
+  my_content <- "^R2$|Std.Errors"
+  my_fmt <- "%.2f"
+  my_rows <- tibble::tribble(~term, ~'OLS', 'Fixed Effect', 'Clustering','Y','Y')
+  attr(my_rows, 'position') <- 5
+  
+  table_tex <- modelsummary::msummary(
+    estimates_lists,
+    gof_omit = my_content,
+    fmt = my_fmt,
+    title = my_title,
+    coef_map = my_varnames,
+    add_rows = my_rows,
+    output = "latex", booktabs = TRUE
+  ) %>% 
+    ##define this function below
+    format_table() %>% 
+    ##define this function below
+    add_double_lines_latex()
+  writeLines(table_tex, my_file_tex)
+  
+  table_image <- modelsummary::msummary(
+    estimates_lists,
+    gof_omit = my_content,
+    fmt = my_fmt, title = my_title,
+    coef_map = my_varnames,
+    add_rows = my_rows,
+    output = "html"
+  ) %>% 
+    ##define this function below
+    format_table() %>% 
+    cat(.,file = my_file_html)
+}
+
+format_tabe <- function(table_input){
+  table_output <- table_input %>% 
+    kableExtra::kable_styling(bootstrap_options = c("hover", "condensed")) %>% 
+    kableExtra::add_header_above(c(" " = 1, "(1)" = 1,"(2)" = 1 )) %>% 
+    kableExtra::kable_classic_2(full_width = F) %>% 
+    kableExtra::footnote(general = "Heteroskedasticity-robust standard error clustered at countries level are reported in the parenthesis.", 
+                        threeparttable = T)
+  
+  return(table_output)
+}
+
+add_double_line <- function(table_input){
+  table_output <- table_input %>% 
+    sub("\\\\toprule","\\\\midrule\\\\midrule",.) %>% 
+    sub("\\\\bottomrule","\\\\midrule\\\\midrule",.)
+  
+  return(table_output)
+}
+
+run_scatter <- function(data_input, x_var, y_var, group_var){
+  x_var <- rlang::enquo(x_var)
+  y_var <- rlang::enquo(y_var)
+  group_var <- rlang::enquo(group_var)
+  
+  require(ggplot2)
+  
+  plot_output <- ggplot(
+    data = data_input,
+    mapping = aes(x = !!x_var, ## !!argumentについてはhttps://psych252.github.io/psych252book/figures/cheatsheets/tidyeval.pdfを参照。
+                  y = !!y_var, ##!!argumetntとした方が人間にとっての可読性が高い
+                  group = !!group_var,
+                  color = !!group_var)
+  ) +
+    geom_point()
+  
+  return(plot_output)
+}
+
+
+source("01_admin/functions/basics.R")
+source("01_admin/functions/elucidate_table.R")
+library(magrittr)
 
 main()
