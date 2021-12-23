209c64ccd1b7bcf3c44b971ea8abbb1b16df1de6
SakagamiKOH <sakagamikoh@gmail.com>
2021-12-24 00:34:05 +0900

    欠損値を含む平均の計算が課題

--- 03_build/pollution_ready/code/build.R
+++ 03_build/pollution_ready/code/build.R
@@ -4,18 +4,18 @@ main <- function(){
   
   ready_data <- tidy_data %>% 
     ##generate standard year table with interval = 5
-    gen_yeartab(interval = 5, var_name = year) 
+    gen_yeartab(interval = 5) 
 
   View(ready_data)
   ##save_interim(ready_data, my_folder, extension = "ready")
 }
 
 
-gen_yeartab <- function(data_input, interval, var_name){
+gen_yeartab <- function(data_input, interval){
 
   data_summarized <- data_input %>% 
     ##interval 年を基準に年を割り振る
-    dplyr::mutate(group = round(year/interval, digits = 0) * 
+    dplyr::mutate(group = round(as.numeric(year)/interval) * 
                     dplyr::if_else(country == "JPN", 
                                    true = 1, 
                                    false = 2, 
@@ -25,14 +25,15 @@ gen_yeartab <- function(data_input, interval, var_name){
     dplyr::ungroup() 
   
   data_output <- data_input %>% 
-    dplyr::mutate(quotient = year/interval) %>% 
-    dplyr::mutate(group = round(quotient, digits = 0) * 
+    dplyr::mutate(quotient = as.numeric(year)/interval) %>% 
+    dplyr::mutate(group = round(quotient) * 
                     dplyr::if_else(country == "JPN", 
                                    true = 1, 
                                    false = 2, 
                                    missing = 3)) %>% 
     ##intervalで割り切れる年の行だけ抽出
     dplyr::filter(quotient %% 1 == 0) %>% 
+    dplyr::select(-pollution) %>% 
     dplyr::left_join(data_summarized, by = ("group" = "group")) %>% 
     dplyr::select(-c("group", "quotient"))
   
