7c7cb73528fc0e263deceb75893090104467cd9e
SakagamiKOH <sakagamikoh@gmail.com>
2021-12-24 00:18:01 +0900

    group変数を作る際に出るエラーにどう対処するか

--- 03_build/pollution_ready/code/build.R
+++ 03_build/pollution_ready/code/build.R
@@ -4,24 +4,37 @@ main <- function(){
   
   ready_data <- tidy_data %>% 
     ##generate standard year table with interval = 5
-    gen_yeartab(interval = 5) %>% 
-    ##select raws with low missing rate
-    select_lowmissing() 
+    gen_yeartab(interval = 5, var_name = year) 
 
-  save_interim(ready_data, my_folder, extension = "ready")
+  View(ready_data)
+  ##save_interim(ready_data, my_folder, extension = "ready")
 }
 
 
-gen_yeartab <- function(data_input, interval){
-  variable <- rlang::enquo(year)
+gen_yeartab <- function(data_input, interval, var_name){
+
+  data_summarized <- data_input %>% 
+    ##interval 年を基準に年を割り振る
+    dplyr::mutate(group = round(year/interval, digits = 0) * 
+                    dplyr::if_else(country == "JPN", 
+                                   true = 1, 
+                                   false = 2, 
+                                   missing = 3)) %>% 
+    dplyr::group_by(group) %>% 
+    dplyr::summarise(pollution = mean(pollution)) %>% 
+    dplyr::ungroup() 
   
   data_output <- data_input %>% 
-    ##interval 年で割り切れる年に絞り込む
-    dplyr::mutate(group = round(year/interval, digits = 0)) %>% 
-    dplyr::group_by(group) %>% 
-    ##ここどうする？
-    dplyr::summarise() %>% 
-    dplyr::ungroup()
+    dplyr::mutate(quotient = year/interval) %>% 
+    dplyr::mutate(group = round(quotient, digits = 0) * 
+                    dplyr::if_else(country == "JPN", 
+                                   true = 1, 
+                                   false = 2, 
+                                   missing = 3)) %>% 
+    ##intervalで割り切れる年の行だけ抽出
+    dplyr::filter(quotient %% 1 == 0) %>% 
+    dplyr::left_join(data_summarized, by = ("group" = "group")) %>% 
+    dplyr::select(-c("group", "quotient"))
   
   return(data_output)  
 }
